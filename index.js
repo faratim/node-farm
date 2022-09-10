5ff252026ec4a9d2d8d8b4fd2a7da6d8e85b6733
faratim <104940465+faratim@users.noreply.github.com>
2022-09-10 14:45:05 -0400

    finished overview card implementation

--- index.js
+++ index.js
@@ -33,22 +33,52 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// this is synchronous/blocking code but it's only executed once at the beginning unlike what's inside the function below which is executed every time a request is made
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
+    
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // Overview Page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the overview.');
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        
+        res.end(output);
+    
+    // Product Page
     } else if (pathName === '/product') {
         res.end('This is the product page.');
     }
+        
+    // API
     else if (pathName === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
     }
+    
+    // Not Found
     else {
         res.writeHead(404, {
             'Content-type': 'text/html'
